c4c1e0bbf0082a54dc886bcab5ac1c3747b0b3fb
soxley <santiago.oxley@gmail.com>
2025-07-11 12:08:08 -0300

    Commit inicial con la configuración del proyecto

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <h1 class="text-3xl font-bold underline mt-12">Bienvenido!</h1>
+    <button class="btn btn-soft btn-info mt-12">Info</button>
+    <button class="btn btn-dash btn-warning mt-12">Warning</button>
+  </div>
+</template>
+
+<style scoped></style>
